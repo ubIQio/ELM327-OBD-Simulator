cecb05dd24f76f89df2d31618d386bc9d2d43750
ubIQio <ubIQio@users.noreply.github.com>
2019-02-14 12:39:01 -0800

    remove case emulation

--- src/Mode01/Response/Fuel/FuelSystemStatus.py
+++ src/Mode01/Response/Fuel/FuelSystemStatus.py
@@ -1,7 +1,6 @@
 import random
 
 from Mode01.BaseClasses.OBDResponse import OBDResponse
-from Utils import Switch
 
 
 class FuelSystemStatus(OBDResponse):
@@ -13,19 +12,18 @@ class FuelSystemStatus(OBDResponse):
                 'System2=' + self.determinestatus(self.byteB)]
 
     def determinestatus(self, byte):
-        for case in Switch(byte):
-            if case('01'):
-                return 'Open loop due to insufficient engine temperature'
-            elif case('02'):
-                return 'Closed loop, using oxygen sensor feedback to determine fuel mix'
-            elif case('04'):
-                return 'Open loop due to engine load OR fuel cut due to deceleration'
-            elif case('08'):
-                return 'Open loop due to system failure'
-            elif case('10'):
-                return 'Closed loop, using at least one oxygen sensor but there is a fault in the feedback system'
-            elif case():
-                return None
+        if byte=='01':
+            return 'Open loop due to insufficient engine temperature'
+        elif byte=='02':
+            return 'Closed loop, using oxygen sensor feedback to determine fuel mix'
+        elif byte=='04':
+            return 'Open loop due to engine load OR fuel cut due to deceleration'
+        elif byte=='08':
+            return 'Open loop due to system failure'
+        elif byte=='10':
+            return 'Closed loop, using at least one oxygen sensor but there is a fault in the feedback system'
+        else:
+            return None
 
     def responsecode(self):
         return '41 03'
